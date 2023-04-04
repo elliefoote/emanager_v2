f4f54472bc65164475ac160d04b002786b0aacf9
Ellie Foote <eleanorjfoote@gmail.com>
2023-04-04 22:04:29 +0200

    rm pword from user obj

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,17 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { UserEntity } from './model/user.entity';
 import { UsersService } from './users.service';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  async findAll() {
+    const users = await this.usersService.findAll();
+    return users.map((user) => new UserEntity(user));
   }
 
   @Get(':email')
-  findOne(@Param('email') email: string) {
-    return this.usersService.findOneByEmail(email);
+  async findOne(@Param('email') email: string) {
+    try {
+      return new UserEntity(await this.usersService.findOneByEmail(email));
+    } catch (err) {
+      throw new NotFoundException('User could not be found!');
+    }
   }
 }
